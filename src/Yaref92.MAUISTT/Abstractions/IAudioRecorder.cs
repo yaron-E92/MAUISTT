291dadf5e8a879e91eb8888c53b37d16ebb35155
Yaron Efrat <yaronefrat92@gmail.com>
2024-07-11 23:01:37 +0200

    [5]: Create the IAudioRecorder

---
+++ src/Yaref92.MAUISTT/Abstractions/IAudioRecorder.cs
@@ -0,0 +1,10 @@
+﻿namespace Yaref92.MAUISTT.Abstractions;
+
+public interface IAudioRecorder
+{
+    void StartRecord(string projectName, string className);
+    void PauseRecord();
+    void ResumeRecord();
+    string StopRecord();
+    void ResetRecord();
+}
