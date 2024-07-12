a303040eff3bad9482e16e59c71c89993c14849e
Yaron Efrat <yaronefrat92@gmail.com>
2024-07-12 13:53:39 +0200

    [5]: Add utils

---
+++ src/Yaref92.MAUISTT/Utils/MediaRecorderState.cs
@@ -0,0 +1,18 @@
+﻿namespace Yaref92.MAUISTT.Utils;
+
+public enum MediaRecorderState
+{
+    /// <summary>
+    /// At this state, the MediaRecorder cannot be used again to record audio
+    /// </summary>
+    Released = -1,
+    /// <summary>
+    /// At this state, the MediaRecorder needs to go through configuration
+    /// of audio source, format, encoding and output path
+    /// </summary>
+    Initial = 0,
+    Reset = Initial,
+    Recording,
+    Paused,
+    Stopped = Initial,
+}
---
+++ src/Yaref92.MAUISTT/Utils/PathUtils.cs
@@ -0,0 +1,23 @@
+﻿#if ANDROID
+using AndroidOS = Android.OS;
+#elif IOS
+using Foundation;
+#endif
+
+namespace Yaref92.MAUISTT.Utils;
+
+public static class PathUtils
+{
+    public static string SetAudioFilePath(string projectName, string className)
+    {
+        string fileName = $"{projectName}_{className}_{DateTime.UtcNow:ddMMM_hhmmss}.mp3";
+        string directoryPath = "";
+#if ANDROID
+        directoryPath = Path.Combine(AndroidOS.Environment.ExternalStorageDirectory.AbsolutePath, AndroidOS.Environment.DirectoryRecordings);
+#elif IOS
+        directoryPath = NSSearchPath.GetDirectories(NSSearchPathDirectory.ApplicationDirectory, NSSearchPathDomain.All)[0];
+#endif
+        string filePath = Path.Combine(directoryPath, fileName);
+        return filePath;
+    }
+}
